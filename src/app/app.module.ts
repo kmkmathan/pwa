46966d7da4dc2f72de5d020c9e92b781427cb6cd
mathankumar k <mathan.k@heptagon.in>
2018-04-22 11:46:24 +0530

    logo and component has added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AraLogoComponent } from './ara-logo/ara-logo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AraLogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ara-logo/ara-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ara-logo',
+  templateUrl: './ara-logo.component.html',
+  styleUrls: ['./ara-logo.component.css']
+})
+export class AraLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
